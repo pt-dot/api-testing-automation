4a2027229db08ac72f3c6265d84f693553951063
Miqdad Farcha <miqdad.farcha@gmail.com>
2020-10-01 00:03:42 +0700

    Register API 📝

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -1,10 +1,23 @@
 <?php
 
-
 namespace App\Repositories;
 
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
 class UserRepository
 {
+    public function registerUser($userData){
+        return User::create([
+            'name' => $userData['name'],
+            'email' => $userData['email'],
+            'password' => Hash::make($userData['password']),
+            'avatar' => $this->generateAvatarUrl($userData['name'])
+        ]);
+    }
 
+    public function generateAvatarUrl($name){
+        return "https://ui-avatars.com/api/?name=" . urlencode($name);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group([
+    'prefix' => 'v1',
+], function(){
+    Route::post('/register', [\App\Http\Controllers\Api\AuthenticationController::class, 'register']);
+});
